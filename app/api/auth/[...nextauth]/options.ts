c88492c86d32ce23b3a68ae6f28abbb853896355
anas599 <anas599@yahoo.com>
2023-07-07 22:12:47 +0300

    add sign in with github option

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,31 @@
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        Credentials({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password", placeholder: "Password" }
+            },
+            async authorize(credentials) {
+                // this is where you would call an API route to authenticate with credentials
+
+                const user = { id: String(1), name: 'JSmith', email: 'smith@mail.com', password: 'password' }
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                }else{
+                    return null
+                }
+            }
+        })
+
+
+    ],
+}
