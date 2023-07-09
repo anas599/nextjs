fe7eae6c549fa1500d30e131786583183027b1a6
anas599 <anas599@yahoo.com>
2023-07-09 12:41:21 +0300

    add test about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,32 +1,8 @@
-import client from '../../lib/db';
-
-client
-  .connect()
-  .then(() => {
-    console.log('Connected to the database2');
-  })
-  .catch((err: any) => {
-    console.error('Error connecting to the database:', err);
-  });
-let arr;
-client
-  .query('SELECT * FROM name')
-  .then((res: any) => {
-    arr = res.rows;
-  })
-  .catch((err: any) => {
-    console.error('Error querying the database:', err);
-  });
-export default function newele() {
-  let arr: never[] = [];
-
-  client
-    .query('SELECT * FROM name')
-    .then((res: { rows: never[] }) => {
-      arr = res.rows;
-    })
-    .catch((err: any) => {
-      console.error('Error querying the database:', err);
-    });
-  console.log(arr);
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+    </div>
+  );
 }
