a92d9374eb863b0edf0184dd0a14f229367d041c
anas599 <anas599@yahoo.com>
2023-07-18 00:21:08 +0300

    clean some code in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
+import Top3 from './component/top3';
+import Header from './component/header';
 
 export default function Component() {
   const { data: session, status } = useSession();
@@ -11,52 +13,10 @@ export default function Component() {
   return (
     <>
       <header className="m-3">
-        <h1 className="text">This is new app in next js</h1>
-        <h2>To display crypto blog along side comments</h2>
-        <p>
-          I have experience in React js, JavaScript, HTML/CSS, Ruby on Rails,
-          and Python, as well as certification in AWS Cloud Practitioner with
-          extensive knowledge in MySQL, PostgreSQL, and DynamoDB. Additionally,
-          I can help optimize your AWS environment for maximum performance and
-          efficiency, with expertise in and .
-        </p>
+        <Header />
       </header>
-      {/* <nav>
-        <Link
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3 my-3"
-          href="/user"
-        >
-          User
-        </Link>
-        {status === 'authenticated' ? (
-          <Link
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3 my-3"
-            href="/api/auth/signout"
-          >
-            Sign Out
-          </Link>
-        ) : (
-          <Link
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3 my-3"
-            href="/api/auth/signin"
-          >
-            Sign In
-          </Link>
-        )}
-        <Link
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3 my-3"
-          href="/about"
-        >
-          About
-        </Link>
-        <p className="m-3"> logged in as: {nameAc}</p>
-        <Link
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-3 my-3"
-          href="/crypto"
-        >
-          Crypto
-        </Link>
-      </nav> */}
+      <h2>Best Cryto Currently</h2>
+      <Top3 />
     </>
   );
 }
