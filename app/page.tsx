4d36fd728c4bec30a9611ca635d3047976911330
anas599 <anas599@yahoo.com>
2023-07-18 13:06:49 +0300

    add animated svg pointer

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,30 @@ export default function Component() {
         <Header />
       </header>
       <h2>Best Cryto Currently</h2>
+      <div className="grid justify-items-center">
+        <svg className="animate-bounce h-20 w-20 m-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            strokeWidth="1.0"
+            stroke="url(#myGradient)"
+          >
+            <defs>
+              <linearGradient id="myGradient" gradientTransform="rotate(90)">
+                <stop offset="0%" stopColor="#9333EA" />
+                <stop offset="100%" stopColor="#3B82F6" />
+              </linearGradient>
+            </defs>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </svg>
+      </div>
+
       <Top3 />
     </>
   );
