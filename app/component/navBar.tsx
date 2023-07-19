2392a64dbddb7d7daef968989ef7e49b94d48fa0
anas599 <anas599@yahoo.com>
2023-07-19 15:29:19 +0300

    update navbar to include user avtar and sign in

--- app/component/navBar.tsx
+++ app/component/navBar.tsx
@@ -1,25 +1,87 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
-
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import * as MaterialDesign from 'react-icons/fa';
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
+  const { data: session, status } = useSession();
 
+  const nameAc = session?.user?.name;
+  const emailAc = session?.user?.email;
+  const [open, setOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  console.log(status);
   const toggleMenu = () => {
     setOpen(!open);
   };
+  const handleToggleDropdown = () => {
+    setIsDropdownOpen((prevIsOpen) => !prevIsOpen);
+  };
 
   return (
-    <nav className="bg-white dark:bg-gray-800">
+    <nav className="bg-white dark:bg-gray-800 fixed z-30 px-4 py-4 w-screen">
       <div className="container mx-auto px-4 md:px-6 lg:px-8">
-        <div className="flex items-center justify-between py-4 md:hidden">
-          {' '}
+        <div className="flex items-center justify-between py-4 md:hidden flex-row-reverse">
+          {/* Drop Down Avatar start mobile*/}
+          <div className="relative">
+            <button
+              id="dropdownUserAvatarButton"
+              onClick={handleToggleDropdown}
+              className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+              type="button"
+            >
+              <span className="sr-only">Open user menu</span>
+              <MaterialDesign.FaUserCircle className="w-8 h-8 text-gray-200" />
+            </button>
+
+            {isDropdownOpen && (
+              <div
+                id="dropdownAvatar "
+                className="z-10 absolute right-0 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <div className="px-4 py-3 text-sm text-gray-900 dark:text-white w-max">
+                  <div>{nameAc}</div>
+                  <div className="font-medium truncate">{emailAc}</div>
+                </div>
+                <ul
+                  className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                  aria-labelledby="dropdownUserAvatarButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Dashboard
+                    </a>
+                  </li>
+                </ul>
+                {status === 'authenticated' ? (
+                  <Link
+                    href="/api/auth/signout"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  >
+                    Sign out
+                  </Link>
+                ) : (
+                  <Link
+                    href="/api/auth/signin"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                  >
+                    Sign in
+                  </Link>
+                )}
+              </div>
+            )}
+          </div>{' '}
+          {/* Drop Down Avatar mobile end */}
+          <h2>{nameAc}</h2>
           <Link
             href="/"
             className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark:text-white focus:outline-none focus:shadow-outline"
-          >
-            Anas
-          </Link>
+          ></Link>
           <button
             className="md:hidden rounded-lg focus:outline-none focus:shadow-outline"
             onClick={toggleMenu}
@@ -69,51 +131,59 @@ const Navbar = () => {
             >
               Contact
             </Link>
-            {/* <div className="relative">
+            {/* Drop Down Avatar start */}
+            <div className="hidden md:block relative">
               <button
-                onClick={toggleMenu}
-                className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark:bg-transparent dark:focus:text-white dark:hover:text-white dark:focus:bg-gray-600 dark:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                id="dropdownUserAvatarButton"
+                onClick={handleToggleDropdown}
+                className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                type="button"
               >
-                <span>Dropdown</span>
-                <svg
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  className={`inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1 ${
-                    open ? 'rotate-180' : 'rotate-0'
-                  }`}
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                <span className="sr-only">Open user menu</span>
+                <MaterialDesign.FaUserCircle className="w-8 h-8 text-gray-200" />
               </button>
-              {open && (
-                <div className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-                  <div className="md:hidden px-2 py-2 bg-white rounded-md shadow dark:bg-gray-800">
-                    <Link
-                      href="#"
-                      className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                    >
-                      Link #1
-                    </Link>
-                    <Link
-                      href="#"
-                      className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                    >
-                      Link #2
-                    </Link>
-                    <Link
-                      href="#"
-                      className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                    >
-                      Link #3
-                    </Link>
+
+              {isDropdownOpen && (
+                <div
+                  id="dropdownAvatar"
+                  className="z-10 absolute right-0 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 "
+                >
+                  <div className="px-4 py-3 text-sm text-gray-900 dark:text-white w-max">
+                    <div>{nameAc}</div>
+                    <div className="font-medium truncate">{emailAc}</div>
                   </div>
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownUserAvatarButton"
+                  >
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Dashboard
+                      </a>
+                    </li>
+                  </ul>
+                  {status === 'authenticated' ? (
+                    <Link
+                      href="/api/auth/signout"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Sign out
+                    </Link>
+                  ) : (
+                    <Link
+                      href="/api/auth/signin"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Sign in
+                    </Link>
+                  )}
                 </div>
               )}
-            </div> */}
+            </div>{' '}
+            {/* Drop Down Avatar end */}
           </div>
         </div>
       </div>
