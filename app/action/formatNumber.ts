187b5bad8c651e83dc59d30234795a8567da62ea
anas599 <anas599@yahoo.com>
2023-07-19 16:30:24 +0300

    add formatter for numbers

---
+++ app/action/formatNumber.ts
@@ -0,0 +1,19 @@
+function formatNumber(num:number, precision:number = 1) {
+const map = [
+    { suffix: 'T', threshold: 1e12 },
+    { suffix: 'B', threshold: 1e9 },
+    { suffix: 'M', threshold: 1e6 },
+    { suffix: 'K', threshold: 1e3 },
+    { suffix: '', threshold: 1 },
+  ];
+
+  const found = map.find((x) => Math.abs(num) >= x.threshold);
+  if (found) {
+    const formatted = (num / found.threshold).toFixed(precision) + found.suffix;
+    return formatted;
+  }
+
+  return num
+}
+console.log(formatNumber(1234567890));
+export default formatNumber;
