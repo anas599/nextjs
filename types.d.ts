ad6876076ab802b40ed3ca46bff9f4d9890fc87a
anas599 <anas599@yahoo.com>
2023-07-25 12:16:38 +0300

    add new types

---
+++ types.d.ts
@@ -0,0 +1,17 @@
+type Crypto1 = {
+    "id": number,
+    "image": String,
+    "name": string,
+    "username": string,
+    "symbol": string,
+    "current_price": number,
+    "total_volume": number,
+    "email": string,
+    "phone": string,
+    "website": string,
+    "company": {
+        "name": string,
+        "catchPhrase": string,
+        "bs": string
+    }
+}
