e38b97fd4c1b0b73aeed3a5c4c59d4d3f90af46b
anas599 <anas599@yahoo.com>
2023-07-27 17:31:42 +0300

    add form styles

--- app/component/addForm.tsx
+++ app/component/addForm.tsx
@@ -12,6 +12,7 @@ function AddForm({ params: { id } }: Params) {
   const { data: session, status } = useSession();
   const nameAc = session?.user?.name;
   const emailAc = session?.user?.email;
+  // @ts-ignore
   const idAc = +session?.user?.id;
   const [content, setComment] = useState('');
 
@@ -45,16 +46,28 @@ function AddForm({ params: { id } }: Params) {
     }
   };
   return (
-    <form onSubmit={handleSubmit} className="z-50 absolute text-zinc-950">
-      <label>
+    <form
+      onSubmit={handleSubmit}
+      className="z-50 absolute text-zinc-950 max-w-screen-2xl"
+    >
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-screen">
         Comment:
         <input
           type="text"
           value={content}
           onChange={(e) => setComment(e.target.value)}
+          className="h-20 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </label>
-      <button type="submit">Submit</button>
+
+      <button
+        type="submit"
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
+        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          Add Comment
+        </span>
+      </button>
     </form>
   );
 }
