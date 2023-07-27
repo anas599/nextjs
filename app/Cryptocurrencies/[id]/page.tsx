9382b95ada9ea2bfdf4ac8e0a0c25b759d6592e9
anas599 <anas599@yahoo.com>
2023-07-27 15:52:54 +0300

    add form and comment section to dynamic page ID

--- app/Cryptocurrencies/[id]/page.tsx
+++ app/Cryptocurrencies/[id]/page.tsx
@@ -1,10 +1,3 @@
-import getSingleCrypto from '@/lib/getSingleCrypto';
-import { prisma } from '@/lib/prisma';
-import { Suspense } from 'react';
-import Image from 'next/image';
-import type { Metadata } from 'next';
-import formatNumber from '../../action/formatNumber';
-
 type Params = {
   params: {
     id: string;
@@ -22,15 +15,36 @@ export async function generateMetadata({
     description: `This is the page of ${crypto.name}`,
   };
 }
+async function getArtist({ params: { id } }: Params) {
+  try {
+    const res = await fetch(`http://localhost:3000/api/`); //when deployed, change to the domain name
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('An error occurred while fetching the data.', error);
+  }
+}
+
+import getSingleCrypto from '@/lib/getSingleCrypto';
+import { Suspense } from 'react';
+
+import Image from 'next/image';
+import type { Metadata } from 'next';
+import formatNumber from '../../action/formatNumber';
+import AddForm from '@/app/component/addForm';
+import PopUpCrypto from '@/app/component/popupCrypto';
 
 export default async function CryptoPage({ params: { id } }: Params) {
   const cryptoData: Promise<Crypto1> = getSingleCrypto(id);
   const crypto = await cryptoData;
-  const oneComment = await prisma.coinComment.findMany({
-    where: {
-      coincommentId: crypto.id.toString(),
-    },
-  });
+  const mycomment = await getArtist({ params: { id } });
+  const filterComment = mycomment.filter(
+    (item: any) => item.coincommentId === id,
+  );
+
   return (
     <>
       <section className="pt-20 grid justify-items-center gap-4 ">
@@ -52,36 +66,36 @@ export default async function CryptoPage({ params: { id } }: Params) {
         </div>
         <Suspense fallback={<h2>Loading...</h2>}></Suspense>
       </section>
+
       <section className="border-double border-4 border-sky-500">
         <div>
           <h2>Comments</h2>
-          {oneComment.map((comment) => (
+          {filterComment.map((comment: any) => (
             <div className="m-3" key={comment.id}>
-              <h2>Date : {comment.createdAt.toDateString()}</h2>
+              <h2>Date : {comment.createdAt}</h2>
               <h3> Comment : {comment.content}</h3>
             </div>
           ))}
         </div>
       </section>
-      <form className="m-3 grid justify-items-center gap-4">
-        <label
-          htmlFor="message"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Your Comment
-        </label>
-        <textarea
-          id="message"
-          rows={4}
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3"
-          placeholder="Leave a comment..."
-        ></textarea>
-        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span className="rounded-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0">
-            Submit
-          </span>
-        </button>
-      </form>
+      <AddForm params={{ id }} />
     </>
   );
+}
+
+export async function getServerProps(context: any) {
+  const id = context.params.id;
+
+  const cryptoData = await getSingleCrypto(id);
+  const mycomment = await getArtist({ params: { id } });
+  const filterComment = mycomment.filter(
+    (item: any) => item.coincommentId === id,
+  );
+
+  return {
+    props: {
+      crypto: cryptoData,
+      mycomment: filterComment,
+    },
+  };
 }
