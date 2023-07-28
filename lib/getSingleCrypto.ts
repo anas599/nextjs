aeff5706c6a19d4f4d9e2af5e9a6c5edc465209b
anas599 <anas599@yahoo.com>
2023-07-28 23:26:17 +0300

    update error message

--- lib/getSingleCrypto.ts
+++ lib/getSingleCrypto.ts
@@ -1,6 +1,6 @@
 export default async function getSingleCrypto(id: string) {
     const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
 
-    if (!res.ok) throw new Error('failed to fetch user')
+    if (!res.ok) throw new Error('failed to fetch Crypto data properly due to API request limit')
     return res.json()
 }
