c5973a0ec4de480dd309c474a4f12dd90beb9769
anas599 <anas599@yahoo.com>
2023-07-29 15:26:48 +0300

    update comment section

--- app/Cryptocurrencies/[id]/page.tsx
+++ app/Cryptocurrencies/[id]/page.tsx
@@ -1,3 +1,13 @@
+import getSingleCrypto from '@/lib/getSingleCrypto';
+import { Suspense } from 'react';
+import Image from 'next/image';
+import type { Metadata } from 'next';
+import formatNumber from '../../action/formatNumber';
+import AddForm from '@/app/component/addForm';
+import PopUpCrypto from '@/app/component/popupCrypto';
+import VoteButton from '@/app/component/voteUpButton';
+import VoteDownButton from '@/app/component/voteDownButton';
+
 type Params = {
   params: {
     id: string;
@@ -17,7 +27,7 @@ export async function generateMetadata({
 }
 async function getComment({ params: { id } }: Params) {
   try {
-    const res = await fetch(`http://localhost:3000/api/`); //when deployed, change to the domain name
+    const res = await fetch(`http://localhost:3000/api/`); //when deployed, change to the domain name or vercel name
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
@@ -27,25 +37,32 @@ async function getComment({ params: { id } }: Params) {
     console.error('An error occurred while fetching the data.', error);
   }
 }
-
-import getSingleCrypto from '@/lib/getSingleCrypto';
-import { Suspense } from 'react';
-
-import Image from 'next/image';
-import type { Metadata } from 'next';
-import formatNumber from '../../action/formatNumber';
-import AddForm from '@/app/component/addForm';
-import PopUpCrypto from '@/app/component/popupCrypto';
-import { FaArrowCircleUp, FaArrowCircleDown } from 'react-icons/fa';
+// async function getAuthorName ({params: {id}}: Params) {
+//   try {
+//     const res = await fetch(`http://localhost:3000/api/Users/listUsers`); //when deployed, change to the domain name or vercel name
+//     if (!res.ok) {
+//       throw new Error(`HTTP error! status: ${res.status}`);
+//     }
+//     const data = await res.json();
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.error('An error occurred while fetching the data.', error);
+//   }
+// }
 
 export default async function CryptoPage({ params: { id } }: Params) {
   const cryptoData: Promise<Crypto1> = getSingleCrypto(id);
   const crypto = await cryptoData;
   const mycomment = await getComment({ params: { id } });
-  const filterComment = mycomment.filter(
-    (item: any) => item.coincommentId === id,
-  );
+  // const authorName = await getAuthorName({ params: { id } });
+  // const filterAuthorName = authorName.filter((item: any) => item.id === id);
+  // console.log(filterAuthorName);
 
+  const filterComment =
+    mycomment?.length === undefined
+      ? []
+      : mycomment?.filter((item: any) => item.coincommentId === id);
   return (
     <>
       <section className="pt-20 grid justify-items-center gap-4 ">
@@ -73,16 +90,15 @@ export default async function CryptoPage({ params: { id } }: Params) {
           <h2>Comments</h2>
           {filterComment.map((comment: any) => (
             <div className="m-3" key={comment.id}>
+              <h2>author : {comment.authorId}</h2>
               <h2>Date : {comment.createdAt}</h2>
               <h3> Comment : {comment.content}</h3>
-              <div className="flex gap-2  items-center ">
-                <div className="flex gap-2 items-center cursor-pointer border-2 border-lime-500 rounded-full p-1 text-lime-500">
-                  <FaArrowCircleUp className="text-lime-500" />
-                  {comment.upvote}
+              <div className="flex gap-2   flex-row items-center ">
+                <div className="flex gap-2 items-center cursor-pointer border-2 text-lime-500 hover:text-white  border-lime-500 hover:bg-lime-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm p-1 text-center mr-2 mb-2 dark:border-lime-500 dark:text-lime-500 dark:hover:text-white dark:hover:bg-lime-600 dark:focus:ring-lime-800">
+                  <VoteButton params={{ comment }} />
                 </div>
-                <div className="flex gap-2  items-center cursor-pointer border-2 border-red-600 rounded-full p-1 text-red-600 ">
-                  <FaArrowCircleDown className="text-red-600" />
-                  {comment.downvote}
+                <div className="flex  flex-row gap-2  items-center cursor-pointer text-red-700 hover:text-white border-2 border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-1 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                  <VoteDownButton params={{ comment }} />
                 </div>
               </div>
             </div>
