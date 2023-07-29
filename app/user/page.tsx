7db97fced3bbab4ff1720490cc0326d3810067fc
anas599 <anas599@yahoo.com>
2023-07-29 15:26:28 +0300

    update user dashboard

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,22 +1,49 @@
-import { prisma } from '@/lib/prisma';
-import Link from 'next/link';
-export default async function user() {
-  const allUsers = await prisma.user.findMany();
+'use client';
+
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { format } from 'date-fns';
+import { useState, useEffect } from 'react';
+
+export default function User() {
+  const [formatExpire, setFormatExpire] = useState('');
+
+  const { data: session, status } = useSession();
+  const nameAc = session?.user?.name;
+  const emailAc = session?.user?.email;
+  useEffect(() => {
+    const expireAc = session?.expires ? new Date(session.expires) : new Date();
+    setFormatExpire(format(expireAc, 'yyyy-MM-dd HH:mm:ss'));
+  }, [session?.expires]);
   return (
-    <div>
-      <Link
-        className="m-5 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-        href="/"
-      >
-        Home
-      </Link>
-      <h1 className="m-3"> This info is fetched from database</h1>
-      {allUsers.map((user) => (
-        <div className="m-3" key={user.id}>
-          <h2>Name : {user.name}</h2>
-          <h3> Email : {user.email}</h3>
+    <section className="pt-28">
+      <h1 className="m-3 text-center font-bold">User Dashboard</h1>
+      <section className="w-max mx-auto bg-[#20354b] rounded-2xl px-8 py-6 shadow-lg text-center flex flex-col items-center shadow-blue-500/20">
+        <div className="flex items-center justify-between"></div>
+        <div className="mt-6 w-fit mx-auto">
+          <Image
+            src={session?.user?.image || '/userIcon.svg'}
+            className="rounded-full w-28 "
+            alt="profile picture"
+            width={80}
+            height={80}
+          />
         </div>
-      ))}
-    </div>
+
+        <div className="mt-8 ">
+          <h2 className="text-white font-bold text-2xl tracking-wide">
+            {nameAc}
+          </h2>
+          <h2 className="text-white font-bold text-2xl tracking-wide">
+            {emailAc}
+          </h2>
+        </div>
+        <p className="text-emerald-400 font-semibold mt-2.5">Active</p>
+        <span className="text-gray-400 text-sm text-center">
+          Session expires at: <br />
+          {formatExpire}
+        </span>
+      </section>
+    </section>
   );
 }
