f49d3c6f4eef385d8b2b6c43e4f78043f88e9dd8
anas599 <anas599@yahoo.com>
2023-07-30 16:37:03 +0300

    get username and profile pic in comment form

--- app/component/addForm.tsx
+++ app/component/addForm.tsx
@@ -11,7 +11,7 @@ type Params = {
 function AddForm({ params: { id } }: Params) {
   const { data: session, status } = useSession();
   const nameAc = session?.user?.name;
-  const emailAc = session?.user?.email;
+  const userPic = session?.user?.image;
   // @ts-ignore
   const idAc = +session?.user?.id;
   const [content, setComment] = useState('');
@@ -21,7 +21,9 @@ function AddForm({ params: { id } }: Params) {
     const commentData = {
       content: content,
       coincommentId: id,
-      authorId: idAc,
+      authorId: 1,
+      username: nameAc,
+      userpic: userPic,
     };
 
     try {
@@ -47,15 +49,15 @@ function AddForm({ params: { id } }: Params) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="z-50 absolute text-zinc-950 max-w-screen-2xl"
+      className="z-50 absolute text-zinc-950  mx-6 my-6 flex flex-col items-center justify-center w-screen"
     >
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-screen">
-        Comment:
+      <label className="flex flex-col justify-center items-center mb-2 text-sm font-medium text-gray-900 dark:text-white w-screen ">
+        Add your Prediction on analysis in a comment :
         <input
           type="text"
           value={content}
           onChange={(e) => setComment(e.target.value)}
-          className="h-20 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="h-20 block p-2.5 w-1/2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         />
       </label>
 
