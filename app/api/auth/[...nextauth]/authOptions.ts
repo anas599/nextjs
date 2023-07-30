6cb6bfdfa7168a853cef47eae2ccfb937b76d65a
anas599 <anas599@yahoo.com>
2023-07-30 23:57:02 +0300

    rename secret varaible

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -58,7 +58,7 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.SECRET,
   callbacks: {
 
     session: ({ session, token }) => {
