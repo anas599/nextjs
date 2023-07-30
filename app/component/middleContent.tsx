e18764be51142db45bf4adb4f1ae2dd91b183da8
anas599 <anas599@yahoo.com>
2023-07-30 16:37:49 +0300

    update middle component and made it responsive

--- app/component/middleContent.tsx
+++ app/component/middleContent.tsx
@@ -5,26 +5,33 @@ const MiddleContent = () => {
     <section className="middleContent mt-10 mb-10 p-5">
       <div
         className="drop-shadow-lg
-        text-center
-        text-5xl
-        font-semibold
-        bg-gradient-to-r
-        bg-clip-text
-        text-transparent
-        from-indigo-500
-        via-purple-500
-        to-indigo-500
-        animate-text
-        h-max
-        p-5
-        border-b-2 rounded-full"
+      text-center
+      text-5xl
+      font-semibold
+      bg-gradient-to-r
+      bg-clip-text
+      text-transparent
+      from-indigo-500
+      via-purple-500
+      to-indigo-500
+      animate-text
+      h-max
+      p-5
+      "
       >
         Features
       </div>{' '}
-      <div className="flex justify-center items-center mt-10 mb-10 pt-10 pb-10">
-        <div className="w-2/4 bg-gradient-to-r from-purple-800 via-violet-900 drop-shadow-md hover:drop-shadow-xl p-2">
-          <h2>Stay Ahead with Crypto Prices & Volume</h2>
-          <p>
+      <div className="flex justify-center">
+        <div className="h-1 rounded-full w-1/3 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500"></div>{' '}
+      </div>
+      <div className="vertical-line"></div>
+      <div className="horizontal-line "></div>
+      <div className="flex flex-col md:flex-row justify-center items-center pt-10 pb-10 shadowDrop border-l-2 border-b-2">
+        <div className="w-full md:w-2/4 bg-gradient-to-r from-purple-800 via-violet-900 drop-shadow-md hover:drop-shadow-xl p-2 md:p-6">
+          <h2 className="text-center md:text-left">
+            Stay Ahead with Crypto Prices & Volume
+          </h2>
+          <p className="text-center md:text-left">
             With our real-time data feed, you will never miss a beat. Keep track
             of market trends and make informed decisions with our unparalleled
             access to cryptocurrency metrics.
@@ -35,13 +42,15 @@ const MiddleContent = () => {
           alt="Picture Chart"
           width={400}
           height={400}
-          className="rounded-3xl drop-shadow-lg "
+          className="rounded-3xl drop-shadow-lg mt-5 md:mt-0"
         />
       </div>
-      <div className="flex justify-center items-center flex-row-reverse mt-10 mb-10 pt-10 pb-10">
-        <div className="flex justify-center items-center flex-col w-2/4 bg-gradient-to-r from-purple-800 via-violet-900 drop-shadow-md hover:drop-shadow-xl p-2">
-          <h2>Stay Ahead with Crypto Prices & Volume</h2>
-          <p>
+      <div className="flex flex-col-reverse md:flex-row justify-center items-center pt-10 pb-10 shadowDrop border-r-2 border-b-2">
+        <div className="flex justify-center items-center flex-col w-full md:w-2/4 bg-gradient-to-r from-purple-800 via-violet-900 drop-shadow-md hover:drop-shadow-xl p-2 md:p-6">
+          <h2 className="text-center md:text-left">
+            Stay Ahead with Crypto Prices & Volume
+          </h2>
+          <p className="text-center md:text-left">
             With our real-time data feed, you will never miss a beat. Keep track
             of market trends and make informed decisions with our unparalleled
             access to cryptocurrency metrics.
@@ -52,17 +61,16 @@ const MiddleContent = () => {
           alt="Picture Chart"
           width={400}
           height={400}
-          className="rounded-3xl drop-shadow-lg"
+          className="rounded-3xl drop-shadow-lg mt-5 md:mt-0"
         />
-        <div />
       </div>
-      <div className="flex justify-center items-center flex-row mt-10 mb-10 pt-10 pb-10">
-        <div className="flex justify-center items-center flex-col w-2/4 bg-gradient-to-r from-purple-800 via-violet-900 drop-shadow-md hover:drop-shadow-xl p-2">
-          <h2>
+      <div className="flex flex-col md:flex-row justify-center items-center pt-10 pb-10 shadowDrop border-l-2">
+        <div className="flex justify-center items-center flex-col w-full md:w-2/4 bg-gradient-to-r from-purple-800 via-violet-900 drop-shadow-md hover:drop-shadow-xl p-2 md:p-6">
+          <h2 className="text-center md:text-left">
             Discuss and add your comments and predictions within the
             cryptocurrency page
           </h2>
-          <p>
+          <p className="text-center md:text-left">
             You can Upvote or downvote any comment in the cryptocurrency page.
           </p>
         </div>
@@ -71,9 +79,8 @@ const MiddleContent = () => {
           alt="Picture Chart"
           width={400}
           height={400}
-          className="rounded-3xl drop-shadow-lg"
+          className="rounded-3xl drop-shadow-lg mt-5 md:mt-0"
         />
-        <div />
       </div>
     </section>
   );
