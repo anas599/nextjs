e9956a580dc3499f81dc6c417fa8a2ff7f642d18
anas599 <anas599@yahoo.com>
2023-08-02 15:57:01 +0300

    add loading animation

--- app/Cryptocurrencies/loading.tsx
+++ app/Cryptocurrencies/loading.tsx
@@ -1,9 +1,9 @@
 function loading() {
   return (
-    <div role="status">
+    <div role="status" className="flex items-center justify-center p-5">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-20 h-20 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
