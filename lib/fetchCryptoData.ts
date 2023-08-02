2dd66a0a4426b859fdca4d6eeeabaac5fffe265e
anas599 <anas599@yahoo.com>
2023-08-02 18:13:16 +0300

    incress fetched crypto

--- lib/fetchCryptoData.ts
+++ lib/fetchCryptoData.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function fetchCryptoData() {
   const apiMarket =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false';
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
   try {
     const response = await axios.get(apiMarket);
