e197896b1948ce0bf52643e9bd20894c9df0291f
anas599 <anas599@yahoo.com>
2023-08-02 15:57:47 +0300

    useEffect and fetch from internal api

--- app/Cryptocurrencies/page.tsx
+++ app/Cryptocurrencies/page.tsx
@@ -1,44 +1,84 @@
-import type { Metadata } from 'next';
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import formatNumber from '../action/formatNumber';
 import getAllCryptos from '@/lib/getAllCryptos';
-export const metadata: Metadata = {
-  title: 'All Cryptocurrencies',
-};
-async function Cryptocurrencies() {
-  const cryptoData: Promise<Crypto[]> = getAllCryptos();
-  const crypto = await cryptoData;
-  return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 justify-items-center items-center content-center pt-20">
-      {crypto.map((crypto: any) => (
-        <Link
-          href="/Cryptocurrencies/[id]"
-          as={`/Cryptocurrencies/${crypto.id}`}
-          key={crypto.id}
-          className="glass flex items-center content-center justify-center"
+import { useEffect, useState } from 'react';
+function Cryptocurrencies() {
+  const [cryptoLength, setCryptoLength] = useState(2);
+  const [crypto, setCrypto] = useState<Crypto[]>([]);
+
+  useEffect(() => {
+    const fetchCryptos = async () => {
+      const cryptoData: Crypto[] = await getAllCryptos();
+      setCrypto(cryptoData.slice(0, cryptoLength));
+    };
+
+    fetchCryptos();
+  }, [cryptoLength]);
+  if (!crypto) {
+    return (
+      <div role="status" className="flex items-center justify-center pt-20">
+        <svg
+          aria-hidden="true"
+          className="w-20 h-20 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <div
-            id={crypto.id}
-            className="grid justify-center glow-blue-100 justify-items-center p-4"
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <section>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 justify-items-center items-center content-center pt-20">
+        {crypto.map((crypto: any) => (
+          <Link
+            href="/Cryptocurrencies/[id]"
+            as={`/Cryptocurrencies/${crypto.id}`}
+            key={crypto.id}
+            className="glass flex items-center content-center justify-center"
           >
-            <Image
-              src={crypto.image}
-              alt={crypto.name}
-              width={50}
-              height={50}
-              className="rounded-lg"
-            />
-            <p>{crypto.name}</p>
-            <p>Symbol: {crypto.symbol}</p>
-            <p>Price: {crypto.current_price} $</p>
-            <p>Volume: {formatNumber(crypto.total_volume)}</p>
-            <p>Market Cap: {formatNumber(crypto.market_cap)}</p>
-            <p>24h Change: {crypto.price_change_percentage_24h}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
+            <div
+              id={crypto.id}
+              className="grid justify-center glow-blue-100 justify-items-center p-4"
+            >
+              <Image
+                src={crypto.image}
+                alt={crypto.name}
+                width={50}
+                height={50}
+                className="rounded-lg"
+              />
+              <p>{crypto.name}</p>
+              <p>Symbol: {crypto.symbol}</p>
+              <p>Price: {crypto.current_price} $</p>
+              <p>Volume: {formatNumber(crypto.total_volume)}</p>
+              <p>Market Cap: {formatNumber(crypto.market_cap)}</p>
+              <p>24h Change: {crypto.price_change_percentage_24h}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <button
+        onClick={() => setCryptoLength(50)}
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+      >
+        <span className="rounded-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0">
+          Buy now!
+        </span>
+      </button>
+    </section>
   );
 }
 
