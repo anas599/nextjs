448b249efc5356b222216af60b83eb0452d0a698
anas599 <anas599@yahoo.com>
2023-08-04 13:43:22 +0300

    incree get crypto to 100

--- lib/getAllCryptos.ts
+++ lib/getAllCryptos.ts
@@ -1,7 +1,9 @@
 async function getAllCryptos() {
-    const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false')
+  const res = await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false',
+  );
 
-    if (!res.ok) throw new Error('failed to fetch data')
-    return res.json()
+  if (!res.ok) throw new Error('failed to fetch data');
+  return res.json();
 }
-export default getAllCryptos
+export default getAllCryptos;
