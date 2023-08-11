184cc2a7475f7eb9a44247bddb719a783ec7f814
anas599 <anas599@yahoo.com>
2023-08-11 14:04:40 +0300

    add exchanges cypress test

---
+++ cypress/e2e/exchanges.cy.js
@@ -0,0 +1,15 @@
+describe('Exchanges page', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    cy.get('a[href*="exchanges"]').first().click();
+    cy.get('input[type="email"]').should('exist').type('anas599@yahoo.com');
+    cy.get('input[type="password"]').should('exist').type('111111');
+    cy.get('input[type="password"]').type('{enter}');
+    cy.url().should('include', '/exchanges');
+    cy.get('h2').should('have.length.at.least', 1);
+    cy.get('p').contains('Spot Volume: $').should('have.length.at.least', 1);
+    cy.get('p').contains('Since: ').should('have.length.at.least', 1);
+  });
+});
