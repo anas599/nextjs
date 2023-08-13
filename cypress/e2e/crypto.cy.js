0c9612c99e1f0032c18af45d27fff551b64eb2af
anas599 <anas599@yahoo.com>
2023-08-13 11:11:11 +0300

    add cypress test to crypto

---
+++ cypress/e2e/crypto.cy.js
@@ -0,0 +1,13 @@
+describe('crypto page', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    cy.get('a[href*="Cryptocurrencies"]').first().click();
+    cy.get('input[type="email"]').should('exist').type('anas599@yahoo.com');
+    cy.get('input[type="password"]').should('exist').type('111111');
+    cy.get('input[type="password"]').type('{enter}');
+    cy.url().should('include', '/Cryptocurrencies');
+    cy.get('p').should('have.length.at.least', 1);
+  });
+});
