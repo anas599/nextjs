7dd243432c0cd9ad0c0972f50fc4d5450916188e
anas599 <anas599@yahoo.com>
2023-08-15 12:18:04 +0300

    update test

--- cypress/e2e/exchanges.cy.js
+++ cypress/e2e/exchanges.cy.js
@@ -1,6 +1,5 @@
 describe('Exchanges page', () => {
   it('should navigate to the about page', () => {
-    // Start from the index page
     cy.visit('http://localhost:3000/');
 
     cy.get('a[href*="exchanges"]').first().click();
