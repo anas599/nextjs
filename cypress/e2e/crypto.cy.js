167aa838de3d220b4360fc490da2f068d85be611
anas599 <anas599@yahoo.com>
2023-08-15 12:17:51 +0300

    remove comment in test

--- cypress/e2e/crypto.cy.js
+++ cypress/e2e/crypto.cy.js
@@ -1,6 +1,5 @@
 describe('crypto page', () => {
   it('should navigate to the about page', () => {
-    // Start from the index page
     cy.visit('http://localhost:3000/');
 
     cy.get('a[href*="Cryptocurrencies"]').first().click();
